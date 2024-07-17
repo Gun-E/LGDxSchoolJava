d292abf972f2b76b19a0fbd5a757793bd3602475
gun <rkdrjs71@naver.com>
2024-07-17 17:40:01 +0900

    커밋

--- ex03_삼항연산자/Ex02.java
+++ ex03_삼항연산자/Ex02.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ex02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("농구공의 개수 입력 : ");
+        System.out.print("농구공의 개수 입력 : ");
         int n = sc.nextInt();
         int result = n % 5 == 0 ? n / 5 : n / 5 + 1;
         System.out.println("필요한 상자의 수 : "+result);
--- ex03_삼항연산자/Ex03.java
+++ ex03_삼항연산자/Ex03.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ex03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("정수를 입력하세요 : ");
+        System.out.print("정수를 입력하세요 : ");
         int n = sc.nextInt();
         String result = n % 2 == 0 ? "홀" : "짝";
         System.out.println(n + "는(은) " + result + "수입니다.");
