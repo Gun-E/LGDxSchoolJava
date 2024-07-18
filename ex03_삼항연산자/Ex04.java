4949f7b2e8cb5c4e6b9269f97c9e19dec3e46f80
gun <rkdrjs71@naver.com>
2024-07-18 14:18:30 +0900

    커밋

--- ex03_삼항연산자/Ex04.java
+++ ex03_삼항연산자/Ex04.java
@@ -7,7 +7,7 @@ public class Ex04 {
         Scanner sc = new Scanner(System.in);
         System.out.print("노동시간을 입력하세요 : ");
         int n = sc.nextInt();
-        int result = n > 8 ? (int)((n - 8) * 5000 + (n - 8) * (5000 * 1.5)) : n * 5000;
-        System.out.println("총임금은" + result + "원 입니다");
+        int result = n > 8 ? (int)(8 * 5000 + (n - 8) * (5000 * 1.5)) : n * 5000;
+        System.out.println("총임금은 " + result + "원 입니다");
     }
 }
