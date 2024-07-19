452621aef765ad4a42f8b05d59b0c00d4e2ed58b
gun <rkdrjs71@naver.com>
2024-07-19 14:13:24 +0900

    커밋

--- ex06_다중if문/Ex07.java
+++ ex06_다중if문/Ex07.java
@@ -1,29 +1,40 @@
 package ex06_다중if문;
 
-import java.util.Map;
+import java.util.List;
 import java.util.Scanner;
 
+class Drink {
+    Drink(String name, int price){
+        this.name = name;
+        this.price = price;
+    }
+    String name;
+    int price;
+}
 public class Ex07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("금액을 입력하세요");
+        System.out.print("금액을 입력하세요 : ");
         int price = sc.nextInt();
 
         System.out.println("메뉴를 고르세요");
         System.out.println("1. 콜라(800원) 2. 생수(500원) 3. 비타민워터(1500원)");
-        Map<Integer, String> map = Map.of(1, "콜라", 2, "생수", 3, "바위");
-        int choice = sc.nextInt();
-        switch (choice) {
-            case 1:
-                System.out.println("콜라를 선택하셨습니다!");
-                break;
-            case 2:
-                System.out.println("생수를 선택하셨습니다!");
-                break;
-            case 3:
-                System.out.println("비타민워터를 선택하셨습니다!");
-                break;
 
+        List<Drink> drinks = List.of(
+                new Drink("콜라", 800),
+                new Drink("생수", 500),
+                new Drink("비타민워터", 1500));
+
+        System.out.println("숫자입력:");
+        int choice = sc.nextInt();
+
+        System.out.println(drinks.get(choice - 1).name + "를 선택하셨습니다!");
+
+        if (drinks.get(choice - 1).price > price) {
+            System.out.println("돈이 부족합니다.");
+            return;
         }
+
+        System.out.println("잔돈 : " + (price - drinks.get(choice - 1).price));
     }
 }
--- ex06_다중if문/re.java
+++ ex06_다중if문/re.java
@@ -1,8 +1,30 @@
 package ex06_다중if문;
 
+import java.util.Scanner;
+
 public class re {
     public static void main(String[] args) {
-        char a = '+';
-        System.out.println(a=='+');
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("금액을 입력하세요 : ");
+        int money = sc.nextInt();
+        System.out.println("메뉴를 고르세요");
+        System.out.println("1. 콜라(800원) 2. 생수(500원) 3. 비타민워터(1500원)");
+        int menu = sc.nextInt();
+        switch (menu) {
+            case 1:
+                System.out.println("콜라를 선택하셨습니다!");
+                break;
+            case 2:
+                System.out.println("생수를 선택하셨습니다!");
+                break;
+            case 3:
+                System.out.println("비타민워터를 선택하셨습니다!");
+                break;
+        }
+        int change = money;
+
+
     }
+
 }
