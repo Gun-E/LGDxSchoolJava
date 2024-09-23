95e8fae7008cc29757a85cd92e654c6d1ca39a0d
gun <rkdrjs71@naver.com>
2024-09-23 15:10:54 +0900

    공부

---
+++ baekjoon/Ex01.java
@@ -0,0 +1,32 @@
+package baekjoon;
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] w = new int[n + 1];
+        int[] v = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            w[i] = sc.nextInt();
+            v[i] = sc.nextInt();
+        }
+
+        int[][] dp = new int[n + 1][k + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                if(w[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                }
+                else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+                }
+            }
+        }
+
+        System.out.println(dp[n][k]);
+    }
+}
