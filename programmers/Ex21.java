6d0ee0d054ede61ebf50733669dcba2996fe476a
gun <rkdrjs71@naver.com>
2024-10-14 10:22:17 +0900

    공부

---
+++ programmers/Ex21.java
@@ -0,0 +1,28 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Ex21 {
+    public static void main(String[] args) {
+        Ex21 ex = new Ex21();
+        System.out.println(ex.solution(new int[][]{{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}}));
+    }
+
+    public int solution(int[][] triangle) {
+        int[][] dp = new int[triangle.length][triangle.length];
+        dp[0][0] = triangle[0][0];
+
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + triangle[i][j];
+                } else if (j == i) {
+                    dp[i][j] = dp[i - 1][j - 1] + triangle[i][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j];
+                }
+            }
+        }
+        return Arrays.stream(dp[triangle.length - 1]).max().orElse(0);
+    }
+}
