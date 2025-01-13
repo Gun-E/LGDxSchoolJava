25a6a2a3214407095c7df49782eb42e18741645b
강건 <rkdrjs71@naver.com>
2025-01-13 19:54:46 +0900

    공부

--- baekjoon/Ex1541.java
+++ baekjoon/Ex1541.java
@@ -7,50 +7,74 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ex1541 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String s = br.readLine();
+
+    /// /        String s = "00009-00009";
+//        String[] split = s.split("");
+//
+//        List<String> list = new ArrayList<>();
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int i = 0; i < split.length; i++) {
+//            if (split[i].equals("-") || split[i].equals("+")) {
+//                list.add(sb.toString());
+//                list.add(split[i]);
+//
+//                sb.delete(0, sb.length());
+//                sb = new StringBuilder();
+//            } else if (i == split.length - 1) {
+//                sb.append(split[i]);
+//                list.add(sb.toString());
+//            } else sb.append(split[i]);
+//        }
+//        int sum = 0;
+//
+//        for (int i = 0; i < list.size(); i++) {
+//            if (list.get(i).equals("-")) {
+//                int miusSum = 0;
+//                while (true) {
+//                    i++;
+//                    if (i == list.size()) break;
+//                    if (list.get(i).equals("-")) {
+//                        break;
+//                    } else if (list.get(i).equals("+")) {
+//                        continue;
+//                    }
+//                    miusSum += Integer.parseInt(list.get(i));
+//                }
+//                sum -= miusSum;
+//                continue;
+//            } else if (list.get(i).equals("+")) {
+//                continue;
+//            }
+//            sum += Integer.parseInt(list.get(i));
+//        }
+//        System.out.println(sum);
+//    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
-//        String s = "00009-00009";
-        String[] split = s.split("");
 
-        List<String> list = new ArrayList<>();
-        StringBuilder sb = new StringBuilder();
+        String[] parts = s.split("-");
 
-        for (int i = 0; i < split.length; i++) {
-            if (split[i].equals("-") || split[i].equals("+")) {
-                list.add(sb.toString());
-                list.add(split[i]);
-
-                sb.delete(0, sb.length());
-                sb = new StringBuilder();
-            } else if (i == split.length - 1) {
-                sb.append(split[i]);
-                list.add(sb.toString());
-            } else sb.append(split[i]);
-        }
         int sum = 0;
 
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).equals("-")) {
-                int miusSum = 0;
-                while (true) {
-                    i++;
-                    if (i == list.size()) break;
-                    if (list.get(i).equals("-")) {
-                        break;
-                    } else if (list.get(i).equals("+")) {
-                        continue;
-                    }
-                    miusSum += Integer.parseInt(list.get(i));
-                }
-                sum -= miusSum;
-                continue;
-            } else if (list.get(i).equals("+")) {
-                continue;
-            }
-            sum += Integer.parseInt(list.get(i));
+        String[] firstParts = parts[0].split("\\+");
+        for (String num : firstParts) {
+            sum += Integer.parseInt(num);
         }
-        System.out.println(sum);
 
+        for (int i = 1; i < parts.length; i++) {
+            String[] nextParts = parts[i].split("\\+");
+            int temp = 0;
+            for (String num : nextParts) {
+                temp += Integer.parseInt(num);
+            }
+            sum -= temp;
+        }
+
+        System.out.println(sum);
     }
 }
